14718f6761ae8b83567a76102247feebb8726116
kiff100 <seth.sterr@gmail.com>
2026-02-18 14:26:24 -0800

    Fixed ship rotation and added it to child object and added rotate toward target

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 
 public class ShipMovement : MonoBehaviour
 {
+    
     private NavMeshAgent agent;
+    private float prefabStartingRotationX;
+    private float prefabstartingRotationY;
+    public float rotationSpeed = 5f; // Rotation speed multiplier
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponentInChildren<NavMeshAgent>();
         agent.updateRotation = false; // Disable automatic rotation
         agent.updateUpAxis = false; // Disable automatic up axis adjustment
     }
@@ -20,6 +24,8 @@ public class ShipMovement : MonoBehaviour
             HandleRightClick();
             Debug.Log("Right mouse button clicked, handling movement.");
         }
+
+        RotateTowardMovement();
     }
 
     private void HandleRightClick()
@@ -30,5 +36,19 @@ public class ShipMovement : MonoBehaviour
 
         agent.SetDestination(mouseWorldPos);
         Debug.Log($"Right-clicked at {mouseWorldPos}, moving ship to that location.");
+    }
+
+    private void RotateTowardMovement()
+    {
+        if (agent.velocity.sqrMagnitude > 0.01f) // Only rotate when moving
+        {
+            Vector3 direction = agent.velocity.normalized;
+            float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            float currentAngle = transform.eulerAngles.z;
+
+            // Smoothly rotate toward the target angle
+            float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, Time.deltaTime * rotationSpeed);
+            transform.eulerAngles = new Vector3(0, 0, newAngle);
+        }
     }
 }
